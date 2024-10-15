752eca3624107fff27b6d04c5cfb9f16c53d2e35
Adams <everleyadams567@gmail.com>
2024-10-15 07:53:32 -0400

    Added an ELIF for my VRS function relating to blizzard cond - stable

--- CastleBravo.py
+++ CastleBravo.py
@@ -17,5 +17,12 @@ def vehicleResponceSystem():
     if weatherAlert == "snowy":
         print("\nThe National Weather Service has Updated our alarm by 30 minutes because"
               "of the forcast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("VRS system has been engaged only allowing you to drive 55mph.")
+    elif weatherAlert == "blizzard":
+        print("\nThe National Weather Service has Updated our alarm by 45 minutes because"
+              "of the forcast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("VRS system has been engaged only allowing you to drive 45mph.")
 
 vehicleResponceSystem()
