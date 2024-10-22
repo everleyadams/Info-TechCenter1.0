e7d816b5cf77901b2971a3166f070e7813e2478a
Adams <everleyadams567@gmail.com>
2024-10-22 08:14:49 -0400

    Added more elifs and eles to the GasLevelAlert Function

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,18 +1,40 @@
 print("\n***************************************************\n")
 
-print("Gasoline Branch")
+print("Gasoline Branch\n")
 
 import random
 from time import sleep
 
 def gasLevelGauge():
-    gasLevelList = ["Empty","Low","Quarter Tank","Half Tanks","Three Quarter Tank", "Full Tank"]
+    gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank", "Full Tank"]
     return random.choice(gasLevelList)
 
 def gasStations():
     gasStationsList = ["VP","Shell","Meijer","Sams Club","Marathon","Mobile","Speedway"]
     return random.choice(gasStationsList)
-print(gasLevelGauge())
-print(gasStations())
 
-#def gasLevelAlert
+
+def gasLevelAlert():
+    milesToGasStationLow = round(random.uniform(1,25),1)
+    milesToGasStationQuarterTank = round(random.uniform(25.1,50),1)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator == "Empty":
+        print("**** WARNING - YOU ARE ON EMPTY***\n")
+        sleep(2)
+        print("Calling Triple AAA")
+    elif gasLevelIndicator == "Low":
+        print("Your gas tank is extremely low, checking GPS for the closest gas station")
+        sleep(2)
+        print("The closest gas station is",gasStations(),"which is",milesToGasStationLow,"miles away.")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("Your gas tank is on a quarter of a tank, checking GPS for the closest gas station")
+        sleep(2)
+        print("The closest gas station is",gasStations(),"which is",milesToGasStationQuarterTank,"miles away.")
+    elif gasLevelIndicator == "Half Tank":
+        print("Your gas tank is half a tank full which is plenty to get to your destination.")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("Your gas tank is three quarter tanks full.")
+    else:
+        print("Your gas tank is full!!! VROOM")
+        
+gasLevelAlert()
